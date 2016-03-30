1573e36ba173804709083d6b2cafc0eeaf681850
mariomelchor <mario@mariomelchor.com>
2016-03-30 12:58:50 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,6 +7,19 @@ function dhali_setup() {
 add_action( 'after_setup_theme', 'dhali_setup' );
 
 /**
+ * Enqueue scripts and styles.
+ */
+function dhali_scripts() {
+	wp_enqueue_style( 'wp-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap-theme.css');
+	wp_enqueue_style( 'wg-style', get_template_directory_uri() . '/css/styles.css');
+
+	wp_enqueue_script( 'google-fonts', get_template_directory_uri() . '/js/google-fonts.js', array(), '', true );
+}
+add_action( 'wp_enqueue_scripts', 'dhali_scripts' );
+	
+/**
  * Load Simplegrid shortcode generator file.
  */
 require get_template_directory() . '/inc/simplegrid.php';
