abab8e6d382dc0ea424ecac28128250369e42133
mariomelchor <mario@mariomelchor.com>
2016-05-09 10:05:09 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ add_action( 'after_setup_theme', 'dhali_setup' );
  * Enqueue scripts and styles.
  */
 function dhali_scripts() {
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
-	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap-theme.css');
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
 	wp_enqueue_style( 'wp-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'wg-style', get_template_directory_uri() . '/css/styles.css');
 
