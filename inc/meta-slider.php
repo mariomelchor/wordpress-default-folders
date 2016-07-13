539b58a64a6eb3ca389e10fcf4d2210dd3e5c6e3
mariomelchor <mario@mariomelchor.com>
2016-07-13 13:44:48 -0700

    Update meta-slider.php

--- inc/meta-slider.php
+++ inc/meta-slider.php
@@ -7,13 +7,13 @@
  */
 
 function dhali_slider_parameters( $params ) {
-		$params['type'] 				= 'flex';
-		$params['width'] 				= 1600;
-		$params['height'] 			= 600;
-		$params['navigation'] 	= 'false';
-		$params['effect'] 			= 'fade';
-		$params['center'] 			= 'true';
-		$params['fullWidth'] 		= 'true';
+	$params['type']		= 'flex';
+	$params['width']	= 1920;
+	$params['height']	= 570;
+	$params['effect'	= 'fade';
+	$params['navigation'] 	= true;
+	$params['center'] 	= true;
+	$params['fullWidth'] 	= true;
 	return $params;
 }
 add_filter('metaslider_default_parameters', 'dhali_slider_parameters', 10, 1);
