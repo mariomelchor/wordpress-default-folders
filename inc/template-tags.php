8e93af5ffdb15bc0f741d513e69632b027f36530
mariomelchor <mario@mariomelchor.com>
2016-08-05 10:58:05 -0700

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -9,3 +9,20 @@ function dhali_the_custom_logo() {
 	}
 }
 endif;
+
+if ( ! function_exists( 'dhali_post_navigation' ) ) :
+/**
+ * Displays the post navigation.
+ * Previous Post/Next Post anchor tags
+ */
+function dhali_post_navigation() {
+	$previous   = get_previous_post_link( '<div class="nav-previous">%link</div>', 'Previous Post', true );
+	$next       = get_next_post_link( '<div class="nav-next">%link</div>', 'Next Post', true );
+
+	// Only add markup if there's somewhere to navigate to.
+	if ( $previous || $next ) {
+		$navigation = _navigation_markup( $previous . $next, 'post-navigation' );
+	}
+	echo $navigation;
+}
+endif;
