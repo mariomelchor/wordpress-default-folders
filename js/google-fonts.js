71e559b7676edf3c55c543d907bffdab470d3184
Mario Melchor <mario@mariomelchor.com>
2017-06-06 14:50:03 -0700

    Updated to new font loader script

--- js/google-fonts.js
+++ js/google-fonts.js
@@ -6,6 +6,6 @@ WebFontConfig = {
 
 (function(d) {
 	 var wf = d.createElement('script'), s = d.scripts[0];
-	 wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+	 wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
 	 s.parentNode.insertBefore(wf, s);
 })(document);
